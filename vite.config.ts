7a50fa46b83c848f9b3a2f44188cca66338eeec1
YusupovWebArt <1artevro1@gmail.com>
2026-06-21 14:05:39 +0200

    build: configure manualChunks code splitting to fix build warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/',
@@ -9,4 +10,19 @@ export default defineConfig({
       overlay: false,
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-icons') || id.includes('lucide-react')) {
+              return 'vendor-icons'
+            }
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
+
